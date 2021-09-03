8568722d3764fc7382179bb3ebbc14200df6e34a
Artur Coelho <artur.rocha.coelho@gmail.com>
2021-09-03 00:30:20 -0300

    Terminado o componente Button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import styles from './style.module.css'
+import LOADING from '../../assets/loading-icon.png'
+import classNames from 'classnames'
 
-const Button = (props) => {
+const Button = ({type, label, subLabel, onClick, isLoading, buttonImage, alt}) => {
   return (
-    <button type="button" className={styles.primaryButton}
-    onClick={props.onClick}>
-      {props.isLoading ? "Loading" : props.label}
-    </button>
+    <>
+    {type === "PRIMARY" && 
+        <button className={styles.primaryButton} onClick={onClick} alt={alt}>
+          {label}
+        </button>
+    }
+
+    {type === "SEND" && 
+        <button alt={alt} className={isLoading ?  classNames(styles.primaryButton, styles.sendButton, styles.loadCursor) : classNames(styles.primaryButton, styles.sendButton)} onClick={onClick}>
+          {isLoading ? <img src={LOADING} alt="" className={styles.loadingImg}></img> : label}
+        </button>
+    }
+
+    {type === "SECONDARY" && 
+        <button alt={alt} className={styles.secondaryButton} onClick={onClick}>
+          <img src={buttonImage} alt=""></img>
+          <div>
+          <p className={styles.buttonTitle}>{label}</p>
+          <small className={styles.buttonText}>{subLabel}</small>
+          </div>
+        </button>
+    }
+
+    </>
   )
 }
 
